f3a3a0ae9ed5e6ef175800d23bfe2a1034ca290b
Ruan <ruan@etus.digital>
2020-10-19 21:22:45 -0300

    avancos

--- Frontend/js/Aluno/main.js
+++ Frontend/js/Aluno/main.js
@@ -1,6 +1,7 @@
 import View from '/js/modules/view.js';
 import Eloquent from "/js/modules/blinder/eloquent.js";
 import Storage from "../modules/storage/storage.js";
+import Blinder from "../modules/blinder/blinder.js";
 
 import Menu from './menu.js';
 import listaTarefas from "./listaTarefas.js";
@@ -19,6 +20,8 @@ const menuView = {
 }
 
 $(document).ready(function (){
+    Blinder.handle()
+
     Menu()
     listaTarefas()
     procurarTarefa()
--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -1,24 +1,29 @@
 import Ajax from "./modules/ajax.js";
 import View from "./modules/view.js";
-import Eloquent from "./modules/blinder/eloquent.js";
 import Blinder from "./modules/blinder/blinder.js";
 import visible from "./modules/visible.js";
 import Storage from "./modules/storage/storage.js";
+import Eloquent from "./modules/blinder/eloquent.js";
 
 $(document).ready(function () {
+    let googleJacu = false
+    let ouvirPrimeiroLogin = false
+    let posAudicaoPrimeiroLogin = false
+    firstLogin();
+
     Blinder.handle();
 
     View.tradeView('loginView');
 
-    $('#cadastrarLink').on('click',function(){
+    $('#cadastrarLink').on('click', function () {
         View.tradeView('cadastroView');
     });
 
-    $('#esqueciSenhaLink').on('click',function(){
+    $('#esqueciSenhaLink').on('click', function () {
         View.tradeView('esqueciView');
     });
 
-    $('.linkVoltarLogin').on('click',function(){
+    $('.linkVoltarLogin').on('click', function () {
         View.tradeView('loginView');
     });
 
@@ -33,12 +38,12 @@ $(document).ready(function () {
             const type = response.codigo === 200 ? 'success' : '#errorCadastrar'
             let speak = response.mensagem
 
-            if(Array.isArray(response))
+            if (Array.isArray(response))
                 speak = response.mensagem.join('<br/>')
 
-            visible.comunicate(speak,type)
+            visible.comunicate(speak, type)
 
-            if(type !== 'success')
+            if (type !== 'success')
                 return false;
 
             return logar(request.emailUsuario, request.senhaUsuario, false)
@@ -49,24 +54,24 @@ $(document).ready(function () {
         logar($('#loginText').val(), $('#senhaText').val())
     });
 
-    function logar (usuario, senha, showMessage = true) {
+    function logar(usuario, senha, showMessage = true) {
         let attributes = {
             'email': usuario,
             'senha': senha,
         };
-        Ajax.setAttributes(attributes).setUrl('login').send(function(response){
+        Ajax.setAttributes(attributes).setUrl('login').send(function (response) {
             const type = response.codigo === 200 ? 'success' : '#errorLogar'
             let speak = 'Seu login foi validado. Entrando no sistema.'
 
-            if(type !== 'success') {
+            if (type !== 'success') {
                 speak = Array.isArray(response.mensagem) ? response.mensagem.join('<br/>') : response.mensagem
-                visible.comunicate(speak,type)
+                visible.comunicate(speak, type)
 
                 return false;
             }
 
-            if(showMessage)
-                visible.comunicate(speak,type)
+            if (showMessage)
+                visible.comunicate(speak, type)
 
             Storage.save('user', response.mensagem)
 
@@ -78,9 +83,57 @@ $(document).ready(function () {
                     default:
                         location.href = '/html/Professor/homeProfessor.html'
                 }
-            },1000);
+            }, 1000);
         });
     }
+
+    function firstLogin() {
+        const first = Storage.get('accessibility')
+
+        if (first !== undefined) {
+            $('#firstLoginContent').addClass('d-none')
+            Blinder.accessibility = first
+            Blinder.handle()
+            return;
+        }
+
+        $('#firstLoginContent').removeClass('d-none')
+        $('#firstLoginContent h3:eq(0)').fadeIn('slow').removeClass('d-none')
+        Eloquent.speak('Seja bem vindo')
+        googleJacu = true
+    }
+
+    $('#firstLoginContent').on('click', function () {
+        if(posAudicaoPrimeiroLogin) {
+            Eloquent.stopListenning(text => {
+                const acessibilidade = text.toLowerCase() == 'sim' ? true : false
+                Blinder.accessibility = acessibilidade
+                Blinder.handle()
+                Storage.save('accessibility', acessibilidade)
+                $('#firstLoginContent').fadeOut('slow')
+            })
+        }
+
+        if(ouvirPrimeiroLogin) {
+            Eloquent.startListenning()
+            ouvirPrimeiroLogin = false
+            posAudicaoPrimeiroLogin = true
+        }
+
+        if (googleJacu) {
+            $('#firstLoginContent h3:eq(1)').fadeIn('slow').removeClass('d-none')
+            Eloquent.speak('Deseja manter o controle de voz ativo ?')
+            setTimeout(() => {
+                $('#firstLoginContent h3:eq(2)').fadeIn('slow').removeClass('d-none')
+            }, 3000)
+            Eloquent.speak('Clique para o começarmos a te ouvir. Depois responda sim ou não e clique novamente para continuar.')
+            setTimeout(() => {
+                $('#firstLoginContent h3:eq(3)').fadeIn('slow').removeClass('d-none')
+            }, 5500)
+            googleJacu = false
+            ouvirPrimeiroLogin = true
+        }
+    })
 })
 
 
